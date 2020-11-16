3dee07fa916231874520d1a7749d444fa36e3681
yao1999 <36053374+yao1999@users.noreply.github.com>
2020-11-16 17:54:02 -0500

    delete user complete

--- Controllers/AdminHomeController.cs
+++ Controllers/AdminHomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Private_Note.Areas.Identity.Data;
@@ -9,6 +10,7 @@ using Private_Note.EncryptAndDecrypt;
 
 namespace Private_Note.Controllers
 {
+    [Authorize]
     public class AdminHomeController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
@@ -26,6 +28,29 @@ namespace Private_Note.Controllers
                 user.SecretPassword = SecretPassword;
             }
             return View(Users);
+        }
+
+        public async Task<IActionResult> DeleteUser([FromForm] string userName)
+        {
+            var currentUser = await _userManager.FindByNameAsync(userName);
+            if (currentUser == null || currentUser.IsAdmin == true)
+            {
+                return RedirectToAction("Index", "AdminHome");
+            }
+            await _userManager.DeleteAsync(currentUser);
+            return RedirectToAction("Index", "AdminHome");
+        }
+
+        public async Task<IActionResult> ChangeSecretPassword([FromForm] string userName, [FromForm] string secretPassword)
+        {
+            var currentUser = await _userManager.FindByNameAsync(userName);
+            if (currentUser == null || currentUser.IsAdmin == true)
+            {
+                return RedirectToAction("Index", "AdminHome");
+            }
+            currentUser.SecretPassword = Methods.Encrypt(secretPassword);
+            await _userManager.UpdateAsync(currentUser);
+            return RedirectToAction("Index", "AdminHome");
         }
 
 
